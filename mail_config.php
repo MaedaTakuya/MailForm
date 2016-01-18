8594e328700cb0c53bd37f4ccd907ce62b9a81f9
前田拓也 <maeda@imgflow.com>
2016-01-18 17:04:51 +0900

    フォームの項目名が見にくいので変更。

--- mail_config.php
+++ mail_config.php
@@ -8,13 +8,13 @@ $redirect_url = "http://xxxx/mail.html";
 // 翻訳用の配列
 // '翻訳したい日本語' => 'formのname属性の値'の形式で記述。
 $translation_list = array(
-  '名前' => 'name',
-  'フリガナ' => 'kana',
-  'メール' => 'mail',
-  'セレクトボックス' => 'selectbox',
-  'ラジオボダン' => 'radio',
-  'チェックボックス' => 'checkbox',
-  '本文' => 'body',
+  '[名前]' => 'name',
+  '[フリガナ]' => 'kana',
+  '[メール]' => 'mail',
+  '[セレクトボックス]' => 'selectbox',
+  '[ラジオボダン]' => 'radio',
+  '[チェックボックス]' => 'checkbox',
+  '[本文]' => 'body',
 );
 
 
